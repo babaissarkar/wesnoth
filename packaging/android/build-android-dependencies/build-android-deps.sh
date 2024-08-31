d6342cdb589218754bc34627757b67745e6454f6
Subhraman Sarkar <suvrax@gmail.com>
2024-08-31 20:27:36 +0530

    download ndk ourselves (action downloading wrong version)

--- packaging/android/build-android-dependencies/build-android-deps.sh
+++ packaging/android/build-android-dependencies/build-android-deps.sh
@@ -34,8 +34,12 @@ ORIGIN=`pwd`
 
 export PREFIXDIR
 
-# Check, show all files here
-ls -lh
+### Download ndk
+wget -n "https://dl.google.com/android/repository/android-ndk-r26d-linux.zip" -P $BUILDDIR
+unzip "$BUILDDIR/android-ndk-r26d-linux.zip" -d $BUILDDIR
+export ANDROID_NDK_HOME="$BUILDDIR/android-ndk-r26d-linux"
+ls -lh $BUILDDIR
+###
 
 python3 setup-toolchains.py
 
