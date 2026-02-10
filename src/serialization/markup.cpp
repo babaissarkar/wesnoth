eadbf5194beb56062b536e368bf7b2d5471e07f6
Subhraman Sarkar <suvrax@gmail.com>
2026-02-10 19:38:50 +0530

    markup: tag -> tag_attr
    also whitespace change in help_topic_generators, markup.cpp

--- src/serialization/markup.cpp
+++ src/serialization/markup.cpp
@@ -24,7 +24,6 @@
 #include "utils/general.hpp"
 
 #include <algorithm>
-#include <sstream>
 
 namespace markup {
 
@@ -469,17 +468,17 @@ static std::string config_to_pango_markup(const std::string& orig_tagname, const
 	std::stringstream text;
 
 	std::string tagname;
-	if (orig_tagname == "bold" || orig_tagname == "b") {
+	if(orig_tagname == "bold" || orig_tagname == "b") {
 		tagname = "b";
-	} else if (orig_tagname == "italic" || orig_tagname == "i") {
+	} else if(orig_tagname == "italic" || orig_tagname == "i") {
 		tagname = "i";
-	} else if (orig_tagname == "underline" || orig_tagname == "u" || orig_tagname == "ref") {
+	} else if(orig_tagname == "underline" || orig_tagname == "u" || orig_tagname == "ref") {
 		tagname = "u"; // <ref> tags will be shown as pango underline
-	} else if (orig_tagname == "format" || orig_tagname == "header" || orig_tagname == "h") {
+	} else if(orig_tagname == "format" || orig_tagname == "header" || orig_tagname == "h") {
 		tagname = "span";
-	} else if (orig_tagname == "img") {
+	} else if(orig_tagname == "img") {
 		return ""; // ignore
-	} else if (orig_tagname == "table") {
+	} else if(orig_tagname == "table") {
 		text << "\n";
 		for (const auto& row : cfg.child_range("row")) {
 			for (const auto& cell : row.child_range("col")) {
@@ -496,17 +495,17 @@ static std::string config_to_pango_markup(const std::string& orig_tagname, const
 	}
 
 	// Inner text content
-	if (cfg.has_attribute("text")) {
+	if(cfg.has_attribute("text")) {
 		text << cfg["text"];
 	}
 
 	// Tag specific formatting attributes
 	tag_attributes attrs;
-	if (orig_tagname == "span" || orig_tagname == "format") {
+	if(orig_tagname == "span" || orig_tagname == "format") {
 		for(const auto& [key, val] : cfg.attribute_range()) {
 			attrs.emplace_back(key, val.str());
 		}
-	} else if (orig_tagname == "header" || orig_tagname == "h") {
+	} else if(orig_tagname == "header" || orig_tagname == "h") {
 		attrs.emplace_back("weight", "heavy");
 		attrs.emplace_back("color", "white");
 		attrs.emplace_back("size", "large");
